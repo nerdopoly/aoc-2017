6eec954b9681931b7faf61cb865365b55688d66a
Adam Birtles <nerdopoly@icloud.com>
2017-12-02 15:51:30 +0000

    Day 2 part 2

--- 02-checksums/main.rs
+++ 02-checksums/main.rs
@@ -1,7 +1,7 @@
 extern crate getinput;
 use getinput::get_input_or_exit;
 
-fn calc_checksum(input: String) -> usize {
+fn calc_checksum(input: &str) -> usize {
     input.lines().map(|line| {
         let nums: Vec<_> = line.split_whitespace().filter_map(|num| {
             num.parse::<usize>().ok()
@@ -11,7 +11,29 @@ fn calc_checksum(input: String) -> usize {
     }).sum()
 }
 
+fn calc_divsum(input: &str) -> usize {
+    input.lines().filter_map(|line| {
+        let mut nums = line.split_whitespace()
+                       .filter_map(|num| {
+                            num.parse::<usize>().ok()
+                       })
+                       .collect::<Vec<_>>();
+        nums.sort_by(|a, b| b.cmp(a)); // reverse sort numbers
+
+        for (i, numerator) in nums.iter().enumerate() {
+            for divisor in &nums[i+1..] {
+                if numerator % divisor == 0 {
+                    return Some(numerator/divisor)
+                }
+            }
+        }
+
+        None
+    }).sum()
+}
+
 fn main() {
     let input = get_input_or_exit();
-    println!("Part 1: {}", calc_checksum(input));
+    println!("Part 1: {}", calc_checksum(&input));
+    println!("Part 2: {}", calc_divsum(&input));
 }
